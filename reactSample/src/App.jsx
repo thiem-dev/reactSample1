586db6f462b8cb167b8d56faa7bb68dd5be3b424
Thiem Huynh <thiem.huynh0@gmail.com>
2023-11-22 17:29:57 -0500

    useState example

--- reactSample/src/App.jsx
+++ reactSample/src/App.jsx
@@ -4,6 +4,9 @@ import './App.css'
 function App() {
   const [todos, setTodos] = useState([])
 
+  if(todos.length === 0){
+    return <h1 id="heading">there are no todos</h1>
+  }
 
   return (
     <h1 id="heading">Hello</h1>
