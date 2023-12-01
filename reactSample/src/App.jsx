aa304a85f6b9c8dbaab8aca401aef7f7b71617c9
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-01 18:00:29 -0500

    basic state change logic

--- reactSample/src/App.jsx
+++ reactSample/src/App.jsx
@@ -2,15 +2,31 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(['call mom'])
+  const [loggedIn, setLoggedIn] = useState(false)
 
-  if(todos.length === 0){
-    return <h1 id="heading">there are no todos</h1>
+
+  const handleClick = () => {
+    // setLoggedIn(true)
+    setLoggedIn(!loggedIn)
+  }
+
+
+  if(!loggedIn){
+    return (
+      <>
+        <h1 id="heading">You are not logged in</h1>
+        <div className='btn' onClick={handleClick}>click here to login</div>
+      </>
+
+    )
   }
 
   return (
-    <h1 id="heading">Hello</h1>
+    <h1 id="heading">Hello, user</h1>
   )
 }
+
+
 
 export default App
