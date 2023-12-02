ed8110747f10dcadc71b694bafcd0ec9c39ff5a8
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-02 17:55:24 -0500

    scrimba play

--- reactSample/src/sample1.jsx
+++ reactSample/src/sample1.jsx
@@ -9,4 +9,12 @@ function MainContent() {
 
 
 
+// const h1Elem = document.createElement("h1");
+// const root = document.getElementById("root")
+
+// h1Elem.textContent = "some content"
+// h1Elem.classList.ad('header')
+// root.appendChild(h1Elem)
+
+
 export default MainContent
