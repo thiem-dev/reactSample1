6a62c390dbedb2810a811e0d45325e337efd48e4
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-02 17:30:59 -0500

    scrimba play

--- reactSample/src/main.jsx
+++ reactSample/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import MainContent from './sample1.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <MainContent />
   </React.StrictMode>,
 )
---
+++ reactSample/src/sample1.jsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+import './App.css'
+
+function MainContent() {
+    return (
+        <h1>Im learning React!</h1>
+    )
+}
+
+
+
+export default MainContent
