1eff844696e1a028e479b1f4dbac4a0702281a5d
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-03 17:25:56 -0500

    component folder update

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import MainContent from './sample1.jsx'
+import MainContent from './components/sample1.jsx'
 import './index.css'
+import Header from './components/Header.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Header />
     {/* <App /> */}
     <MainContent />
   </React.StrictMode>,
