7bf1a5caa557fb9b12fdff85264fe1ad2d0b9989
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-02 19:09:58 -0500

    scrimba play

--- reactSample/src/sample2.jsx
+++ reactSample/src/sample2.jsx
@@ -1,15 +1,19 @@
-// scrimba uses 17 instead of 18. createRoot is better
-import React from "react"
-import ReactDOM from "react-dom"
+// scrimba uses old 17 syntax
+import React from "react";
+import  ReactDOM  from "react-dom";
 
-const page = (
-    <div>
-        <img src="./react-logo.png" width="40px" />
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>released in 2013</li>
-        </ul>
-    </div>
-)
 
-ReactDOM.render(page, document.getElementById("root"))
+function PageEle() {
+    return (
+        <div>
+            <h1>react facts</h1>
+            <ul>
+                <li>this is cool</li>
+            </ul>
+        </div>
+    )
+}
+
+
+const root = document.getElementById('root')
+ReactDOM.render(<PageEle />, root)
