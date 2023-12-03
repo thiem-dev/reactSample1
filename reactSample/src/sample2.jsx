a5d727c697947c9803ed15d73770c6a868f8f7c8
Thiem Huynh <thiem.huynh0@gmail.com>
2023-12-02 19:10:55 -0500

    scrimba play

--- reactSample/src/sample2.jsx
+++ reactSample/src/sample2.jsx
@@ -1,19 +1,29 @@
 // scrimba uses old 17 syntax
-import React from "react";
-import  ReactDOM  from "react-dom";
 
 
-function PageEle() {
+import React from "react"
+import ReactDOM from "react-dom"
+
+function Page() {
     return (
         <div>
-            <h1>react facts</h1>
-            <ul>
-                <li>this is cool</li>
-            </ul>
+            <header>
+                <nav>
+                    <img src="./react-logo.png" width="40px" />
+                </nav>
+            </header>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+            <footer>
+                <small>© 2021 Ziroll development. All rights reserved.</small>
+            </footer>
         </div>
     )
 }
 
-
-const root = document.getElementById('root')
-ReactDOM.render(<PageEle />, root)
+ReactDOM.render(<Page />, document.getElementById("root"))
